5b7d126e993ded03793c4c0db0b69de164831acf
RISHIKESHGUPTA2004 <acgupta2004@gmail.com>
2026-09-21 01:43:11 +0530

    edit index.js

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,11 +1,11 @@
-//centeralized API setup
-
 import axios from 'axios';
 import qs from 'qs';
 
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5173'; 
+
 export const axiosInstance = axios.create({
-    baseURL: '/api',
+    baseURL: BASE_URL,
     withCredentials: true,
     paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }),
 })
-
