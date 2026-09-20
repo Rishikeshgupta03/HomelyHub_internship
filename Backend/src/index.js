3e7c40850c405c4b97b1bc383230bf987d4c418a
RISHIKESHGUPTA2004 <acgupta2004@gmail.com>
2026-09-21 01:25:35 +0530

    edit index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -11,6 +11,16 @@ import { tripRouter} from "./routes/tripRouter.js";
 
 const app = express();
 
+app.use(cors({
+    origin: [
+        "http://localhost:5173",                 
+        "https://homelyhubbooking.netlify.app"   
+    ],
+    credentials: true,                           
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
